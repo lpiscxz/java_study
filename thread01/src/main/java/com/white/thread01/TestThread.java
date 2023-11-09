e62abce05d81c67178ea6318c52e991f1dc6185b
whitebird <1804355625@qq.com>
2023-11-09 15:18:44 +0800

    Initial commit

---
+++ thread01/src/main/java/com/white/thread01/TestThread.java
@@ -0,0 +1,10 @@
+package com.white.thread01;
+
+/**
+ * @Auther: lupei
+ * @Date: 2023/11/9 - 11 - 09 - 14:20
+ * @Description: com.white.thread01
+ * @version: 1.0
+ */
+public class TestThread {
+}
